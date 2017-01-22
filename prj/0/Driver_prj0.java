b75d214d4928dad38036fa00495524cab32eaea9
johnatawnclementawn <JohnathanClementi@148-100-183-113.FoxNet.marist.edu>
2017-01-22 01:53:58 -0500

    This is V1.2 of the program that displays Hello World

--- prj/0/Driver_prj0.java
+++ prj/0/Driver_prj0.java
@@ -3,7 +3,8 @@
  * author: Johnathan Clementi
  * course: CMPT 220 Software Development 1, Section 201
  * due date: Tuesday, January 24th, 2017
- * version: 1.0
+ * version: 1.2
+ *   New version fixes misspellings.
  *
  * This file contains the main method for displaying the
  * string "Hello World!".
