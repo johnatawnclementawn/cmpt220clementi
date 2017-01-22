5a210d535d9f3f7636552869b09c856fc62325aa
johnatawnclementawn <JohnathanClementi@148-100-183-113.FoxNet.marist.edu>
2017-01-22 01:23:13 -0500

    This is V1 of the program that displaus Hello World

---
+++ prj/0/Driver_prj0.java
@@ -0,0 +1,16 @@
+/**
+ * file: Driver_prj0
+ * author: Johnathan Clementi
+ * course: CMPT 220 Software Development 1, Section 201
+ * due date: Tuesday, January 24th, 2017
+ * version: 1.0
+ *
+ * This file contains the main method for displaying the
+ * string "Hello World!".
+ */
+
+public class Driver_prj0 {
+  public static void main(String[] args){
+    System.out.println("Hello World!");
+  }
+}
