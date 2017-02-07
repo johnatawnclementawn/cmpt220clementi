9258cebc62141a7e1e014e6f82fa60afe02cb312
johnatawnclementawn <JohnathanClementi@148-100-131-98.FoxNet.marist.edu>
2017-02-07 00:56:08 -0500

    This update to Listing 2.10 in the Intro To Java Programming book fixes the loss of accuracy when converting from a float to an integer value.

---
+++ labs/2/Problem4_26.java
@@ -0,0 +1,56 @@
+/**
+ * file: Problem 4.26
+ * author: Johnathan Clementi
+ * course: CMPT 220 Software Development 1, Section 201
+ * due date: Tuesday, February 7th, 2017
+ * version: 0.1
+ *
+ * This file contains the methodology for a program that 
+ * 
+ *
+ */
+
+import java.util.Scanner;
+
+public class Problem4_26 {
+  public static void main(String[] args){
+
+    Scanner input = new Scanner(System.in);
+    
+    // Convert input from double value to string value
+    System.out.println("Enter an amount in double, for example 11.56: ");   
+    String amount = input.nextLine();
+
+    // Dollar and cent strings seperated into two seperate values to be analyzed.
+    String dollars = amount.substring(0 , amount.indexOf("."));
+    String cents = amount.substring(amount.indexOf(".") + 1, amount.length());
+
+    //Convert each string to an integer
+    int numberOfOneDollars = Integer.parseInt(dollars);
+    int remainingAmount = Integer.parseInt(cents);
+
+    //Find  the number of quarters in the remaining amount
+    int numberOfQuarters = remainingAmount / 25;
+    remainingAmount = remainingAmount % 25;
+
+    //Find the number of dimes in the remaining amount
+    int numberOfDimes = remainingAmount / 10;
+    remainingAmount = remainingAmount % 10;
+
+    //Find the number of nickels in the remaining amount
+    int numberOfNickels = remainingAmount / 5;
+    remainingAmount = remainingAmount % 5;
+
+    //Find the number of pennies in the remaining amount
+    int numberOfPennies = remainingAmount;
+
+    //Display results
+    System.out.println("Your amount " + amount + " consists of");
+    System.out.println("    " + numberOfOneDollars + " dollars");
+    System.out.println("    " + numberOfQuarters + " quarters");
+    System.out.println("    " + numberOfDimes + " dimes");
+    System.out.println("    " + numberOfNickels + " nickels");
+    System.out.println("    " + numberOfPennies + " pennies");
+    
+  }
+}
