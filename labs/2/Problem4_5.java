99a69a765142024f063010d9e6d17551ef45ad5f
johnatawnclementawn <JohnathanClementi@148-100-131-98.FoxNet.marist.edu>
2017-02-06 19:15:50 -0500

    Completed: area equation and user input. To do: check if equation is correct, and make do/while to check for negative input values.

---
+++ labs/2/Problem4_5.java
@@ -0,0 +1,34 @@
+/**
+ * file: Problem 4.5
+ * author: Johnathan Clementi
+ * course: CMPT 220 Software Development 1, Section 201
+ * due date: Tuesday, February 7th, 2017
+ * version: 0.1
+ *
+ * This file contains the methodology for a program that 
+ * calculates the area of a regular polygon based on a user
+ * inputed number of sides and length of those sides.
+ */
+
+import java.util.Scanner;
+
+public class Problem4_5 {
+  public static void main(String[] args){
+
+  Scanner input = new Scanner (System.in);
+
+  int numSide = 0;
+  double sideLength = 0.0;
+  double area = 0.0;
+
+  System.out.println("Enter the number of sides: ");
+  numSide = input.nextInt();
+
+  System.out.println("Enter the length of the sides: ");
+  sideLength = input.nextDouble();
+  
+  area = ((numSide * (Math.pow(sideLength, 2))) / (4 * (Math.tan(Math.PI / numSide))));
+
+  System.out.println("The area of the polygon is: " + area);
+  }
+}
