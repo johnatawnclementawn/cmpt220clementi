b2c23506a8bbb27dc06cdbd039a962061c1a78d1
johnatawnclementawn <JohnathanClementi@148-100-161-202.FoxNet.marist.edu>
2017-02-18 17:19:58 -0500

    Updated comments, file description, and changed a variable names for clarity.  There is a logic issue with calculating the total tuition after 4 years.

--- labs/3/Problem5_7.java
+++ labs/3/Problem5_7.java
@@ -5,29 +5,27 @@
  * due date: Tuesday, February 21, 2017
  * version: 0.1
  *
- * This file contains the methodology for a program that 
- *
+ * This file contains the methodology for a program that calculates
+ * the anual increase of tuition by 5% and calculates the total of 4 years
+ * tuition after 10 years has passed.
  */
-
-import java.util.Scanner;
 
 public class Problem5_7 {
   public static void main(String[] args){
-    
-    Scanner input = new Scanner(System.in);
-    
+
     double tuition = 10000.0;
     double totCost = 0.0;
 
-    // For loop 
-    for (int i = 0; i < 14; i++) {
-      // 
+    // For loop repeates calculations until the year has reached 14
+    for (int year = 0; year < 14; year++) {
+      // Increases tutition for each year
       tuition = (tuition * 1.05);
 
-      // 
-      if (i == 9){
+      // Calculates just the increase in tuition for the first 9 years, then
+      // adds 4 years of tuition
+      if (year == 9){
         System.out.format("The tuition for the 10th year is: %.2f\n", tuition);
-      } else if (i > 9) {
+      } else if (year > 9) {
           totCost = (totCost + tuition);
       }
     }
