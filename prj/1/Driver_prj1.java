00d4033968c4f3ede5bda8ef87f6e79c9eaf64c3
johnatawnclementawn <JohnathanClementi@148-100-147-22.FoxNet.marist.edu>
2017-03-25 20:10:07 -0400

    This program reduces noise of two vectors and carries our the convolution algorithm.

--- prj/1/Driver_prj1.java
+++ prj/1/Driver_prj1.java
@@ -22,16 +22,26 @@ public class Driver_prj1 {
     double[] vFirst = new double[vFirstLen];
     double[] vSecond = new double[vSecondLen];
 
+    // Input values of first function
     while (i < vFirstLen){
       vFirst[i] = input.nextDouble();
       i++;
     }
     
+    // Reset I to 0 and input values of second function
     i = 0;
     while (i < vSecondLen){
       vSecond[i] = input.nextDouble();
       i++;
     }
+
+    double[] cResult = convolveVectors(vFirst,vSecond);
+
+    // Round each output to the nearest integer and display 
+    for (i = 0; i < (vFirst.length + vSecond.length - 1); i++){
+      System.out.print(Math.round(cResult[i]) + ", ");
+    }
+    System.out.print("\n");
   }
 
   /**
@@ -40,13 +50,25 @@ public class Driver_prj1 {
    * This function switches the order of the numbers of an array
    *
    * Parameters:
-   *   A - array of user inputed numbers to be sorted
+   *   vFirst - the first of the vectors to be convolved
+   *   vSecond - the second of the vectors to be convolved 
    *
-   * Return value: temp
+   * Return value: vResult
    */
   public static double[] convolveVectors(double[] vFirst, double[] vSecond) {
+    // Set the length of the convoled vector 
     double[] vResult = new double[(vFirst.length + vSecond.length) - 1];
-    
+      
+      for (int idx = 0; idx < (vFirst.length + vSecond.length - 1); idx++) {
+        vResult[idx] = 0;
+
+        for (int shift = 0; shift < vSecond.length; shift++) {
+          
+          if ((idx - shift >= 0) && (idx - shift < vFirst.length)) {
+            vResult[idx] += (vFirst[idx - shift] * vSecond[shift]);
+          }
+        }
+      }
     return vResult;  
   }
 }
